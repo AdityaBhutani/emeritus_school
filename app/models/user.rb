5162bdfc9df4caa30eacecaed0e31b13fded3a3c
Aditya Bhutani <adi_bhutani16@yahoo.in>
2024-02-28 17:00:42 +0530

    Devise setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'is not a valid email' }, uniqueness: true
--- db/migrate/20240226151029_create_users.rb
+++ db/migrate/20240226151029_create_users.rb
@@ -5,12 +5,9 @@ class CreateUsers < ActiveRecord::Migration[7.1]
     create_table :users do |t|
       t.string :first_name, null: false
       t.string :last_name, null: false
-      t.string :email, null: false
       t.string :type
 
       t.timestamps
     end
-
-    add_index :users, :email, unique: true
   end
 end
