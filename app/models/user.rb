7ea523e5f7f51f257ca2e56944aeae3506482a9c
Aditya Bhutani <adi_bhutani16@yahoo.in>
2024-02-29 16:18:51 +0530

    SchoolsController with views

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'is not a valid email' }, uniqueness: true
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
