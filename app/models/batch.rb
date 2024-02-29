244981dea99a7a1d987eee1c970e4d4e99fb0112
Aditya Bhutani <adi_bhutani16@yahoo.in>
2024-02-29 21:35:41 +0530

    Fix for association

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -7,5 +7,5 @@ class Batch < ApplicationRecord
 
   belongs_to :course
   has_many :enrollments, dependent: :destroy
-  has_many :students, through: :enrollments, source: :user, source_type: 'Student'
+  has_many :students, through: :enrollments, source: :student
 end
