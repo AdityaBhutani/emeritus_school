8a5b1433f55e0177b8c2641edcbdb34ba8608e18
Aditya Bhutani <adi_bhutani16@yahoo.in>
2024-03-01 06:36:30 +0530

    SchoolAdminPolicy

---
+++ spec/policies/school_admin_policy_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe SchoolAdminPolicy do
+  subject { described_class.new(user, school_admin) }
+
+  let(:school_admin) { create(:school_admin) }
+
+  context 'when the user is an Admin' do
+    let(:user) { create(:admin) }
+
+    it 'permits access to index' do
+      expect(subject.index?).to be_truthy
+    end
+
+    it 'permits access to show' do
+      expect(subject.show?).to be_truthy
+    end
+
+    it 'permits creating a school admin' do
+      expect(subject.create?).to be_truthy
+    end
+
+    it 'permits access to new' do
+      expect(subject.new?).to be_truthy
+    end
+
+    it 'permits updating a school admin' do
+      expect(subject.update?).to be_truthy
+    end
+
+    it 'permits access to edit' do
+      expect(subject.edit?).to be_truthy
+    end
+
+    it 'permits destroying a school admin' do
+      expect(subject.destroy?).to be_truthy
+    end
+  end
+
+  context 'when the user is not an Admin' do
+    let(:user) { nil } # Example where user is not set
+
+    it 'denies access to all actions' do
+      expect(subject.index?).to be_falsey
+      expect(subject.show?).to be_falsey
+      expect(subject.create?).to be_falsey
+      expect(subject.new?).to be_falsey
+      expect(subject.update?).to be_falsey
+      expect(subject.edit?).to be_falsey
+      expect(subject.destroy?).to be_falsey
+    end
+  end
+end
