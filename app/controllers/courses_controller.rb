48ae51c8b03cf1f39f01118ab70a0416c770e6ab
Aditya Bhutani <adi_bhutani16@yahoo.in>
2024-03-01 07:16:55 +0530

    CoursesControllerSpec

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class CoursesController < ApplicationController
-  before_action :set_course, only: %i[ show edit update destroy ]
-  before_action :set_schools, only: %i[ new edit]
-  before_action :authorize_user, only: %i[ show edit update destroy ]
+  before_action :set_course, only: %i[show edit update destroy]
+  before_action :set_schools, only: %i[new edit]
+  before_action :authorize_user, only: %i[show edit update destroy]
 
   # GET /courses or /courses.json
   def index
@@ -10,8 +12,7 @@ class CoursesController < ApplicationController
   end
 
   # GET /courses/1 or /courses/1.json
-  def show
-  end
+  def show; end
 
   # GET /courses/new
   def new
@@ -19,8 +20,7 @@ class CoursesController < ApplicationController
   end
 
   # GET /courses/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /courses or /courses.json
   def create
@@ -29,7 +29,7 @@ class CoursesController < ApplicationController
 
     respond_to do |format|
       if @course.save
-        format.html { redirect_to course_url(@course), notice: "Course was successfully created." }
+        format.html { redirect_to course_url(@course), notice: 'Course was successfully created.' }
         format.json { render :show, status: :created, location: @course }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -42,7 +42,7 @@ class CoursesController < ApplicationController
   def update
     respond_to do |format|
       if @course.update(course_params)
-        format.html { redirect_to course_url(@course), notice: "Course was successfully updated." }
+        format.html { redirect_to course_url(@course), notice: 'Course was successfully updated.' }
         format.json { render :show, status: :ok, location: @course }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -56,27 +56,28 @@ class CoursesController < ApplicationController
     @course.destroy!
 
     respond_to do |format|
-      format.html { redirect_to courses_url, notice: "Course was successfully destroyed." }
+      format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_course
-      @course = Course.find(params[:id])
-    end
 
-    def set_schools
-      @schools = School.all
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_course
+    @course = Course.find(params[:id])
+  end
 
-    def authorize_user
-      authorize @school
-    end
+  def set_schools
+    @schools = School.all
+  end
 
-    # Only allow a list of trusted parameters through.
-    def course_params
-      params.require(:course).permit(:name, :description, :school_id)
-    end
+  def authorize_user
+    authorize @course
+  end
+
+  # Only allow a list of trusted parameters through.
+  def course_params
+    params.require(:course).permit(:name, :description, :school_id)
+  end
 end
